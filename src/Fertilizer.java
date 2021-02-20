f7c0e3644c0bfa85c129f232a2c414b350169870
Radoslaw Choluj <radekcholuj@wp.pl>
2021-02-20 18:17:56 +0100

    #1 dodanie menu, dodanie klasy flower i filitizer, dodanie klasy product i zasrosowanie dziedziczenia z ta klasa, sparwzdanie czy jeste jeszcze wolne miejsce na kolejne produkty

---
+++ src/Fertilizer.java
@@ -0,0 +1,12 @@
+public class Fertilizer extends Product {
+    private String purpose;
+
+    public Fertilizer(String name, double price, String purpose) {
+        super(name, price);
+        this.purpose = purpose;
+    }
+
+    public String getPurpose() {
+        return purpose;
+    }
+}
